fe406c7c578a0b9ba06ea7c89277afa24161ce1e
doroshenkoam <spartacity@mail.ru>
2021-11-15 23:57:04 +0300

    [+] added base button

---
+++ lib/const.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// TODO: цвета поискать получше в макетах.
+
+// colorBG цвет заднего фона.
+const colorBG = Color(0xFF111328);
+// colorBGAppBar цвет заднего фона app бара.
+const colorBGAppBar = Color(0xFF111320);
+
+// styleTextAppBar стиль текста app бара.
+const styleTextAppBar = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xAA999911),
+);
+
+// colorSelectThemeButton цвет кнопок выбора тем.
+const colorSelectThemeButton = Color(0xFF111315);
+// colorInStoreButton цвет кнопки машазина.
+const colorInStoreButton = Color(0xAD940123);
+
+// styleTextInStoreButton стиль текста кнопки захода в магазин.
+// TODO: сделать текст жирнее.
+const styleTextInStoreButton = TextStyle(
+  fontSize: 20.0,
+  wordSpacing: 8,
+  color: Color(0xAA999911),
+);
